821b1c795fe3cfd150cd5baaf2a578a49d16e669
milselarch <milselarch@gmail.com>
2022-06-04 22:06:18 +0800

    fuzzy autocomplete

--- dest-search/src/App.vue
+++ dest-search/src/App.vue
@@ -16,21 +16,81 @@
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
     </ul>
+
+    <br/>
+    <section>
+      <b-field label="Find a name" id="searchbox">
+        <b-autocomplete
+          v-model="name"
+          :data="filteredDataArray"
+          placeholder="e.g. jQuery"
+          icon="magnify"
+          clearable
+          @select="option => selected = option">
+          <template #empty>No results found</template>
+        </b-autocomplete>
+      </b-field>
+    </section>
   </div>
 </template>
 
 <script>
+import destinations from './assets/destinations.json'
+import fuzzysort from 'fuzzysort'
+
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'Welcome to Your Vue.js App',
+      destinationNames: [],
+      name: '',
+      selected: null
+    }
+  }, 
+  mounted: function () {
+    const destinationNames = []
+    for (let destination of destinations) {
+      // console.log(destination)
+      this.destinationNames.push(destination["term"])
+    }
+  },
+  computed: {
+    filteredDataArray() {
+      const results = fuzzysort.go(this.name, this.destinationNames)
+      if ((results.length) === 0) { return [] }
+      
+      const names = []
+      const length = Math.min(results.length, 100)
+      for (let k=0; k<length; k++) {
+        console.log(k, results[k])
+        names.push(results[k]['target'])
+      }
+      return names
     }
   }
 }
 </script>
 
 <style lang="scss">
+* {
+  font-family: 'Open Sans', sans-serif;
+}
+
+#searchbox {
+  width: 20rem;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+@font-face {
+  font-family: "Open Sans";
+  src:
+    url("./assets/fonts/open_sans.ttf") format("truetype");
+    /* Add other formats as you see fit */
+}
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- dest-search/webpack.config.js
+++ dest-search/webpack.config.js
@@ -18,6 +18,15 @@ module.exports = {
         ],
       },
       {
+        test: /\.(ttf|otf|eot|woff|woff2)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "fonts/[name].[ext]",
+          },
+        },
+      },
+      {
         test: /\.scss$/,
         use: [
           'vue-style-loader',
