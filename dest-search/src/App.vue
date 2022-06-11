964d7c8cc15ed181f610eb4374fc91ab25ece76d
milselarch <milselarch@gmail.com>
2022-06-11 11:11:57 +0800

    add navbar

--- dest-search/src/App.vue
+++ dest-search/src/App.vue
@@ -1,5 +1,36 @@
 <template>
   <div id="app">
+    <b-navbar id="main-navbar">
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/wdssvw2kkzjjiagwckjf"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          >
+        </b-navbar-item>
+      </template>
+
+      <template #end>
+        <b-navbar-item tag="div">
+          <b-navbar-item href="#">
+            Home
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            About Us
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Features
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Contact
+          </b-navbar-item>
+        </b-navbar-item>
+      </template>
+    </b-navbar>
+
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+
     <section>
       <b-field label="Search for a Destination" id="searchbox">
         <b-autocomplete
@@ -49,6 +80,11 @@ import destinations from './assets/destinations.json'
 import fuzzysort from 'fuzzysort'
 import sleep from 'await-sleep'
 import axios from 'axios'
+
+const routes = {
+  '/': Home,
+  '/about': About
+}
 
 export default {
   name: 'app',
@@ -166,14 +202,19 @@ div#hotel-cards {
     /* Add other formats as you see fit */
 }
 
-
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
+
+  & > nav#main-navbar.navbar {
+    padding-left: 2rem !important;
+    padding-right: 2rem !important;
+    padding: 1rem;
+  }
 }
 
 h1, h2 {
