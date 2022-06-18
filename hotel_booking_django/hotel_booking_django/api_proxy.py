07af530b577d32eaafa5452edad389a6af34f031
milselarch <milselarch@gmail.com>
2022-06-18 18:35:19 +0800

    handle non-json responses for backend proxy

--- hotel_booking_django/hotel_booking_django/api_proxy.py
+++ hotel_booking_django/hotel_booking_django/api_proxy.py
@@ -1,4 +1,6 @@
 import requests
+import traceback
+import json
 
 from django.views.decorators.csrf import csrf_exempt
 from django.http import JsonResponse
@@ -14,17 +16,28 @@ def proxy_view(request, path):
     # print('params', get_params)
     remote_url = f'{base_url}/{path}?{formatted_get_params}'
     proxy_success, error_message = True, ''
-    response_json = {}
+    response_json, raw_response = {}, ''
+    response, status = None, -1
 
     try:
         response = requests.get(remote_url)
-        response_json = response.json()
     except requests.exceptions.RequestException as e:
+        traceback.print_exc()
         error_message = str(e)
         proxy_success = False
+
+    if response is not None:
+        status = response.status_code
+
+        try:
+            response_json = response.json()
+        except json.decoder.JSONDecodeError:
+            raw_response = response.content.decode()
 
     return JsonResponse({
         'proxy_success': proxy_success,
         'error_message': error_message,
-        'proxy_response': response_json
+        'proxy_json': response_json,
+        'proxy_raw_response': raw_response,
+        'status_code': status
     })
