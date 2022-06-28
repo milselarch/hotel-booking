7a08cf619f5ba7118ea5b2a9f5bf3f3ba72f1f69
Joe Baarath <joebaarath@hotmail.com>
2022-06-28 18:34:38 +0800

    added last name to front end, back end & unit tests

--- hotel_booking_django/accounts/serializers.py
+++ hotel_booking_django/accounts/serializers.py
@@ -6,4 +6,4 @@ User = get_user_model()
 class UserCreateSerializer(UserCreateSerializer):
     class Meta(UserCreateSerializer.Meta):
         model = User
-        fields = ('uid', 'email', 'first_name', 'password')
+        fields = ('uid', 'email', 'first_name', 'last_name', 'password')
