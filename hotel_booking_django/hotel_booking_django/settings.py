3fc7c2ba4f65d8cfc1a8c069b7b45ed5ddf089c0
Daniel Koh <aikhong_koh@mymail.sutd.edu.sg>
2022-07-04 14:34:55 +0800

    changed serializer used for auth/users/me endpoint

--- hotel_booking_django/hotel_booking_django/settings.py
+++ hotel_booking_django/hotel_booking_django/settings.py
@@ -165,6 +165,7 @@ DJOSER = {
         'user_create': 'accounts.serializers.UserCreateSerializer',
         'user': 'accounts.serializers.UserCreateSerializer',
         'user_delete': 'djoser.serializers.UserDeleteSerializer',
+        "current_user": 'accounts.serializers.UserCreateSerializer',
     }
 }
 
