83260d4a8389848b7306117842e50c7155ce5fb9
Daniel Koh <aikhong_koh@mymail.sutd.edu.sg>
2022-07-17 00:03:15 +0800

    implemented encryption for sensitive data fields

--- hotel_booking_django/payment/models.py
+++ hotel_booking_django/payment/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 from common.models import common_attribute_model, country_code
 from accounts.models import user_account
+from django_cryptography.fields import encrypt
 
 class user_payment(common_attribute_model):
     payment_mode = models.PositiveIntegerField(validators=[MinValueValidator(10000000),MaxValueValidator(999999999999999)], blank=True, null=True)
@@ -9,14 +10,14 @@ class user_payment(common_attribute_model):
     is_primary_payment_mode = models.BooleanField(default = False, blank=True, null=True)
 
 class user_payment_credit_card_details(common_attribute_model):
-    name_on_card = models.CharField(max_length=255, blank=True, null=True)
-    card_number = models.CharField(max_length=20, blank=True, null=True)
-    expiry_date = models.DateField(blank=True, null=True)
-    security_code = models.IntegerField(blank=True, null=True)
-    billing_address_address = models.CharField(max_length=255, blank=True, null=True)
+    name_on_card = encrypt(models.CharField(max_length=255, blank=True, null=True))
+    card_number = encrypt(models.CharField(max_length=20, blank=True, null=True))
+    expiry_date = encrypt(models.DateField(blank=True, null=True))
+    security_code = encrypt(models.CharField(max_length=3, blank=True, null=True))
+    billing_address_address = encrypt(models.CharField(max_length=255, blank=True, null=True))
     billing_address_country = models.ForeignKey(country_code, on_delete=models.PROTECT, blank=True, null=True)
-    billing_address_city = models.CharField(max_length=100, blank=True, null=True)
-    billing_address_post_code = models.CharField(max_length=100, blank=True, null=True)
+    billing_address_city = encrypt(models.CharField(max_length=100, blank=True, null=True))
+    billing_address_post_code = encrypt(models.CharField(max_length=100, blank=True, null=True))
     user_payment = models.ForeignKey(user_payment, on_delete=models.PROTECT, blank=True, null=True)
 
 
