4a81031a957e831450723a8342378f7bb5d2d7f8
milselarch <milselarch@gmail.com>
2022-07-19 13:02:27 +0800

    add login unit tests

--- dest-search/tests/unit/Signup.spec.js
+++ dest-search/tests/unit/Signup.spec.js
@@ -36,28 +36,26 @@ localVue.use(Vuex)
 localVue.use(Buefy)
 
 describe('Signup Test', () => {
-  let timestamp, date_stamp;
-  let username, password;
-  let email, first_name, last_name;
+  const date_now = new Date()
+  const timestamp = date_now.getTime();
+  const date_stamp = moment(date_now).format(
+    'YYMMDD-hhmmss-SSS'
+  );
+  
+  // generate form data for signup and login tests
+  const username = `test-user-${date_stamp}`
+  const email = `test-email-${date_stamp}@testmail.com`
+  const password = crypto.randomBytes(6).toString('hex');
+  const first_name = crypto.randomBytes(2).toString('hex');
+  const last_name = crypto.randomBytes(2).toString('hex');
+
+  console.log('DATE_STAMP', date_stamp)
+  console.log('PASSWROD', password, first_name, last_name)
 
   let signed_up = false;
   let store;
 
   beforeEach(() => {
-    const date_now = new Date()
-    timestamp = date_now.getTime();
-    date_stamp = moment(date_now).format('YYMMDD-hhmmss-SSS');
-    
-    // generate form data for signup and login tests
-    username = `test-user-${date_stamp}`
-    email = `test-email-${date_stamp}@testmail.com`
-    password = crypto.randomBytes(6).toString('hex');
-    first_name = crypto.randomBytes(2).toString('hex');
-    last_name = crypto.randomBytes(2).toString('hex');
-
-    console.log('DATE_STAMP', date_stamp)
-    console.log('PASSWROD', password, first_name, last_name)
-
     // trick axios into thinking its running on the browser
     // otherwise all requests will report with NetworkError
     axios.defaults.adapter = require('axios/lib/adapters/http');
@@ -160,11 +158,16 @@ describe('Signup Test', () => {
     const event_data = wrapper.emitted('open-login')
     // console.log('EVENT', event_data)
     expect(event_data).toStrictEqual([[formdata]])
-    console.log('SIGNUP COMPLETE')
+    console.log('SIGNUP COMPLETE', formdata)
     signed_up = true;
   })
 
   it('login success test', async () => {
+    /*
+    test that login is successfull if we enter the correct
+    email and correct password
+    */
+
     const start_stamp = (new Date()).getTime() / 1000
     while (!signed_up) {
       // wait for the signup test to complete
@@ -204,5 +207,61 @@ describe('Signup Test', () => {
     // ensure login-done event is emitted
     const event_data = wrapper.emitted('login-done')
     expect(event_data).toStrictEqual([[formdata]])
+  })
+
+  it('login password failure test', async () => {
+    /*
+    test that login fails if we enter the correct
+    email but wrong password
+    */
+    const start_stamp = (new Date()).getTime() / 1000
+    while (!signed_up) {
+      // wait for the signup test to complete
+      // and ensure there exists a user account in the backend
+      // from which we can login into
+      await sleep(100);
+    }
+
+    const end_stamp = (new Date()).getTime() / 1000
+    const duration = end_stamp - start_stamp
+    console.log('WAIT DURATION', duration)
+
+    const wrapper = mount(Login, {
+      store, localVue,
+      //specify custom components
+      stubs: stubs
+    })
+
+    const login_button = wrapper.find('#login');
+    const errors_box = wrapper.find('#errors_box')
+    expect(login_button.exists()).toBe(true);
+    expect(wrapper.vm.has_error).toBe(false);
+    // const email_field = wrapper.find('#email-field')
+    // const password_field = wrapper.find('#password-field')
+
+    let false_password = password
+    while (false_password === password) {
+      // keep generating random differing passwords
+      // the while loop makes sure the regenerated password
+      // does not end up marching the original password
+      // I know this is unbelievably improbable, but whatever
+      false_password = crypto.randomBytes(6).toString('hex');
+    }
+
+    wrapper.vm.email = email
+    wrapper.vm.password = false_password
+    await wrapper.vm.$nextTick()
+
+    expect(wrapper.vm.pending).toBe(false)
+    await login_button.vm.$listeners.click()
+    expect(wrapper.vm.pending).toBe(true)
+    // wait for the sign up axios request to complete
+    while (wrapper.vm.pending) { await sleep(100); }
+
+    expect(wrapper.vm.has_error).toBe(true);
+    const errors = wrapper.vm.other_errors;
+    const err_msg = 'No active account found with the given credentials'
+    expect(errors).toBe(err_msg)
+    // console.log('LOGIN ERRORS', errors)
   })
 })
