c9c7b8b0650c4fb3578fae8a6bb6e458553d7f89
Daniel Koh <aikhong_koh@mymail.sutd.edu.sg>
2022-07-21 17:41:59 +0800

    updated layout for profile page

--- dest-search/src/components/Profile.vue
+++ dest-search/src/components/Profile.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tile is-ancestor">
+  <div class="tile is-ancestor details">
 
     <div class="tile is-parent">
       <article class="tile is-child notification is-primary">
@@ -8,7 +8,7 @@
           <p class="subtitle"></p>
           <div class="content">
             <div id="load-status">
-              <div id="status" v-show="is_loading">
+              <div id="status" v-show="!load_success">
                 <p id="status-text">{{ status_text }}</p>
                 <square id="spinner" v-show="is_loading"></square>
                 <b-button 
@@ -22,8 +22,8 @@
             </div>
           </div>
           <div id="account_data">
-            <p> {{ email }} </p>
-            <p> {{ first_name }} {{ last_name }} </p>
+            <p> Email: {{ email }} </p>
+            <p>Name: {{ first_name }} {{ last_name }} </p>
           </div>
 
         </div>
@@ -35,9 +35,11 @@
 
         <div class="tile is-parent is-vertical">
           <article class="tile is-child notification is-success">
-
+            <p class="title">Payment Information</p>
+            <p class="subtitle">List of payment methods saved under your account</p>
           </article>
           <article class="tile is-child notification is-warning">
+            <p class="title">Edit Account</p>
             <b-button
               label="Delete account"
               type="is-danger"
@@ -64,11 +66,58 @@
 
         <div class="tile is-parent">
           <article class="tile is-child notification is-info">
-            <p class="title">Middle tile</p>
-            <p class="subtitle">With an image</p>
-            <figure class="image is-4by3">
-              <img src="https://bulma.io/images/placeholders/640x480.png">
-            </figure>
+            <p class="title">Booking Details</p>
+            <p class="subtitle">The most recent booking will be shown</p>
+            <div class="content">
+              <p>
+                Booking ID: <b-tag>{{ booking_id }}</b-tag>
+              </p>
+              <p>
+                Booking creation date: <b-tag>{{ datetime_created }}</b-tag> 
+              </p>
+              <p>
+                Destination ID: <b-tag>{{ destination_id }}</b-tag>
+              </p>
+              <p> 
+                Hotel ID: <b-tag>{{ hotel_id }}</b-tag> 
+              </p>
+              <p>
+                Room Type ID: <b-tag>{{ room_type_id }}</b-tag>  
+              </p>
+              <p>
+                Check In Date: <b-tag>{{ check_in_date }}</b-tag> 
+              </p>
+              <p>
+                Checkout Date: <b-tag>{{ check_out_date }}</b-tag> 
+              </p>
+              <p>
+                Number of rooms: <b-tag>{{ number_of_rooms }}</b-tag>
+              </p>
+              <p>
+                Number of guests per room: <b-tag>{{ number_of_guests_per_rooms }}</b-tag>
+              </p>
+              <p>
+                Special request: <b-tag>{{ special_request }}</b-tag> 
+              </p>
+              <p>
+                Primary Guest: <b-tag>{{ primary_guest_title }} {{ primary_guest_first_name }} {{ primary_guest_last_name }}</b-tag>
+              </p>
+              <p>
+                Primary guest email: <b-tag>{{ primary_guest_email }}</b-tag> 
+              </p>
+              <p>
+                Primary guest phone: <b-tag>{{primary_guest_phone_country}} {{ primary_guest_phone }}</b-tag>
+              </p>
+              <p>
+                Cost of booking (SGD): <b-tag>{{ cost_in_sgd }}</b-tag> 
+              </p>
+              <p>
+                Booking terms and condition: <b-tag>{{ booking_tnc }}</b-tag> 
+              </p>
+              <p>
+                Hotel terms and condition: <b-tag>{{ hotel_tnc }}</b-tag>
+              </p>
+          </div>
           </article>
         </div>
 
@@ -158,6 +207,27 @@ export default {
       email: null,
 
       isDeleteAccountModalActive: false,
+
+      booking_id: null,
+      destination_id: null,
+      hotel_id: null,
+      room_type_id: null,
+      booking_id: null,
+      check_in_date: null,
+      check_out_date: null,
+      number_of_rooms: null,
+      number_of_guests_per_rooms: null,
+      special_request: null,
+      primary_guest_title: null,
+      primary_guest_first_name: null,
+      primary_guest_last_name: null,
+      primary_guest_email: null,
+      primary_guest_phone: null,
+      primary_guest_phone_country: null,
+      cost_in_sgd: null,
+      datetime_created: null,
+      booking_tnc: null,
+      hotel_tnc: null,
     }
   },
 
@@ -194,12 +264,14 @@ export default {
       self.load_success = false;
       const requester = new AuthRequester(self)
       let response;
+      let responseBooking;
       
       (async () => {
         console.log('LOAD REQ START')
 
         try {
           response = await requester.get('profile')
+          responseBooking = await requester.get("booking/")
           self.load_success = true
         } catch (error) {
           const status_code = error.response.status;
@@ -221,10 +293,34 @@ export default {
         }
 
         console.log('RESPONSE', response);
+        console.log('Booking Response', responseBooking);
         self.status_text = 'profile info'
         self.first_name = response.data.first_name
         self.last_name = response.data.last_name
         self.email = response.data.email
+
+        let last = responseBooking.data.length-1
+        console.log("last", last)
+        self.booking_id = responseBooking.data[last].uid
+        self.destination_id = responseBooking.data[last].destination_id
+        self.hotel_id = responseBooking.data[last].hotel_id
+        self.room_type_id = responseBooking.data[last].room_type_id
+        // self.booking_id = responseBooking.data[last].booking_id
+        self.check_in_date = responseBooking.data[last].check_in_date
+        self.check_out_date = responseBooking.data[last].check_out_date
+        self.number_of_rooms = responseBooking.data[last].number_of_rooms
+        self.number_of_guests_per_rooms = responseBooking.data[last].number_of_guests_per_rooms
+        self.special_request = responseBooking.data[last].special_request
+        self.primary_guest_title = responseBooking.data[last].primary_guest_title
+        self.primary_guest_first_name = responseBooking.data[last].primary_guest_first_name
+        self.primary_guest_last_name = responseBooking.data[last].primary_guest_last_name
+        self.primary_guest_email = responseBooking.data[last].primary_guest_email
+        self.primary_guest_phone = responseBooking.data[last].primary_guest_phone
+        self.primary_guest_phone_country = responseBooking.data[last].primary_guest_phone_country
+        self.cost_in_sgd = responseBooking.data[last].cost_in_sgd
+        self.datetime_created = responseBooking.data[last].datetime_created.split("T")[0]
+        self.booking_tnc = responseBooking.data[last].booking_tnc
+        self.hotel_tnc = responseBooking.data[last].hotel_tnc
       })();
 
       return true;
@@ -312,22 +408,10 @@ div#load-status {
   }
 }
 
-div.profile {
-  display: flex;
-  flex-direction: column;
-
-  margin-left: auto;
-  margin-right: auto;
-  width: fit-content;
-  margin-top: 2rem;
-
-  & div#profile-info {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-top: 2rem;
-    justify-content: center;
-  }
+.details{
+  padding-top: 2rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
 }
 
 p {
