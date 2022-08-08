a2b58cb15f8a218b6892b56085759989d9cc0ca3
Daniel Koh <aikhong_koh@mymail.sutd.edu.sg>
2022-08-08 21:24:54 +0800

    changed ttl of access token

--- hotel_booking_django/hotel_booking_django/settings.py
+++ hotel_booking_django/hotel_booking_django/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -172,6 +173,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
    'USER_ID_FIELD': 'uid',
+   'ACCESS_TOKEN_LIFETIME': timedelta(minutes=21),
 }
 
 DJOSER = {
